615db8af3c14759eb5024f97c4137c7c30c40c7d
Ashley Casey <acasey.dev@gmail.com>
2017-09-05 00:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function subtract(a, b) {
 function multiply(a, b) {
   return (a * b);
 }
+
+function divide(a, b) {
+  return (a / b);
+}
+
+function inc(n) {
+  return (n += 1);
+}
+
+function dec(n) {
+  return (n -= 1);
+}
+
+function makeInt(n) {
+  parseInt(n, 10);
+  return n;
+}
